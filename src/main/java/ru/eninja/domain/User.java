9b91337655b897a823e579c90f8020fdc6b278c9
kolya-t <toporkov.kolya96@gmail.com>
2017-02-16 21:47:00 +0300

    User transfer object

---
+++ src/main/java/ru/eninja/domain/User.java
@@ -0,0 +1,82 @@
+package ru.eninja.domain;
+
+import javax.persistence.*;
+
+
+/**
+ * User Transfer Object
+ */
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    public User() {
+    }
+
+    public User(String login, String password, String email, String role) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public User(Long id, String login, String password, String email, String role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) return false;
+        if (!login.equals(user.login)) return false;
+        if (!password.equals(user.password)) return false;
+        if (!email.equals(user.email)) return false;
+        return role.equals(user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + login.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
