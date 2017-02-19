4b08ddd8ec5d772893c5f1a7eba2db5515370a7c
kolya-t <toporkov.kolya96@gmail.com>
2017-02-19 18:38:25 +0300

    Add: UserService interface

---
+++ src/main/java/ru/eninja/service/UserService.java
@@ -0,0 +1,65 @@
+package ru.eninja.service;
+
+import ru.eninja.domain.User;
+
+import java.util.List;
+
+
+/**
+ * Interface of abstract user service. Defines operations on users
+ */
+public interface UserService {
+
+    /**
+     * Saves user to table
+     *
+     * @param user user to save
+     * @return id of saved user
+     */
+    Long addUser(User user);
+
+    /**
+     * Finds and returns user by id
+     *
+     * @param id user id
+     * @return user by id
+     */
+    User getUserById(Long id);
+
+    /**
+     * Finds and returns user list
+     *
+     * @return user list
+     */
+    List<User> getUserList();
+
+    /**
+     * Updates user in table
+     *
+     * @param user user to update
+     */
+    void updateUser(User user);
+
+    /**
+     * Deltes user by id
+     *
+     * @param id user id
+     */
+    void deleteUser(Long id);
+
+    /**
+     * Return user by login
+     *
+     * @param login user login
+     * @return user by login
+     */
+    User getUserByLogin(String login);
+
+    /**
+     * Return user by email
+     *
+     * @param email user email
+     * @return user by email
+     */
+    User getUserByEmail(String email);
+}
