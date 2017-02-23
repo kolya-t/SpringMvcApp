934683fb1943f65034028ca18bdcae22f66186ed
kolya-t <toporkov.kolya96@gmail.com>
2017-02-23 17:10:40 +0300

    add: role

---
+++ src/main/java/ru/eninja/domain/Role.java
@@ -0,0 +1,74 @@
+package ru.eninja.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Integer id = -1;
+
+    @Column(name = "name", nullable = false, unique = true)
+    @NotNull
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (!id.equals(role.id)) return false;
+        return name.equals(role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
