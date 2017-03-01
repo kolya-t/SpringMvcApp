2a86cd05785d5d081e124ffb4e9aa750adbad354
kolya-t <toporkov.kolya96@gmail.com>
2017-03-01 16:24:46 +0300

    Added CustomUserDetailsService

---
+++ src/main/java/ru/eninja/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package ru.eninja.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.eninja.domain.User;
+import ru.eninja.service.UserService;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserByLogin(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getLogin(),
+                user.getPassword(),
+                user.getEnabled(),
+                true, true, true,
+                user.getRole()
+        );
+    }
+
+
+}
