f3f8ceb7d44d06cef72546629c4d1548b85e6e05
kolya-t <toporkov.kolya96@gmail.com>
2017-03-04 18:09:42 +0300

    Fixed bug

--- src/main/java/ru/eninja/domain/Role.java
+++ src/main/java/ru/eninja/domain/Role.java
@@ -33,7 +33,7 @@ public class Role implements GrantedAuthority {
 
     @Override
     public String getAuthority() {
-        return null;
+        return authority;
     }
 
     public void setAuthority(String authority) {
@@ -46,5 +46,28 @@ public class Role implements GrantedAuthority {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null) return false;
+        return authority != null ? authority.equals(role.authority) : role.authority == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (authority != null ? authority.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return this.authority;
     }
 }
