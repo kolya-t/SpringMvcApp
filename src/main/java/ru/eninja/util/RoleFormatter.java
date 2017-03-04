cb3855ebb949ab2ba7e1d9c5cbbffec0ff10295e
kolya-t <toporkov.kolya96@gmail.com>
2017-03-04 18:13:41 +0300

    Parsing by getting role from roleService

--- src/main/java/ru/eninja/util/RoleFormatter.java
+++ src/main/java/ru/eninja/util/RoleFormatter.java
@@ -1,20 +1,29 @@
 package ru.eninja.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import ru.eninja.domain.Role;
+import ru.eninja.service.RoleService;
 
 import java.text.ParseException;
 import java.util.Locale;
 
 
 public class RoleFormatter implements Formatter<Role> {
+
+    @Autowired
+    private RoleService roleService;
+
     @Override
-    public Role parse(String id, Locale locale) throws ParseException {
+    public Role parse(String idStr, Locale locale) throws ParseException {
         try {
-            Role role = new Role();
-            role.setId(Integer.parseInt(id));
+            int id = Integer.parseInt(idStr);
+            Role role = roleService.getRoleById(id);
+            if (role == null) {
+                throw new ParseException("Role not found", 0);
+            }
             return role;
-        } catch (NumberFormatException e) {
+        } catch (Exception e) {
             throw new ParseException(e.getMessage(), 0);
         }
     }
