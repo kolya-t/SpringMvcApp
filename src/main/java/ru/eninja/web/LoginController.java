08e67eec235363ff173eab0de27243dff763978e
kolya-t <toporkov.kolya96@gmail.com>
2017-03-04 19:19:59 +0300

    Added signing up

--- src/main/java/ru/eninja/web/LoginController.java
+++ src/main/java/ru/eninja/web/LoginController.java
@@ -1,29 +1,65 @@
 package ru.eninja.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import ru.eninja.domain.Role;
+import ru.eninja.domain.User;
+import ru.eninja.service.RoleService;
+import ru.eninja.service.UserService;
+
+import javax.validation.Valid;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView login(
-            @RequestParam(value = "error", required = false) String error,
-            @RequestParam(value = "logout", required = false) String logout) {
+    @Autowired
+    private UserService userService;
 
-        ModelAndView modelAndView = new ModelAndView("login");
+    @Autowired
+    private RoleService roleService;
+
+    @RequestMapping(value = "/login", method = GET)
+    public String showLogin(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout,
+            Model model) {
 
         if (error != null) {
-            modelAndView.addObject("error", "Invalid login or password");
+            model.addAttribute("errorMessage", "Invalid login or password");
         }
         if (logout != null) {
-            modelAndView.addObject("msg", "You've been logged out successfully.");
+            model.addAttribute("successMessage", "You've been logged out successfully.");
         }
 
-        return modelAndView;
+        return "login";
+    }
+
+    @RequestMapping(value = "signup", method = GET)
+    public String showSignup(Model model) {
+        model.addAttribute(new User());
+        return "signup";
+    }
+
+    @RequestMapping(value = "signup", method = POST)
+    public String signup(@Valid User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        user.setRoles(new HashSet<>(Arrays.asList(roleService.getRoleByAuthority("ROLE_USER"))));
+        userService.addUser(user);
+        return "redirect:/login";
     }
 }
